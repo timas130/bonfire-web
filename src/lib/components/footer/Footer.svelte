de184ee0e0fea28504e5d832e70c2ab1a57f1487
sit <me@sit.sh>
2024-01-22 22:27:01 +0300

    fix website on mobile

--- src/lib/components/footer/Footer.svelte
+++ src/lib/components/footer/Footer.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <Container el="footer" class="">
-  <div class="flex justify-center items-center group gap-6 text-lg py-6 opacity-70 hover:opacity-100 transition">
+  <div class="flex flex-col lg:flex-row justify-center items-center group gap-6 text-lg py-6 opacity-70 hover:opacity-100 transition">
     <FooterLink href="/page/privacy">Политика конфиденциальности</FooterLink>
     <FooterLink href="/page/terms">Условия использования</FooterLink>
     <FooterLink href="/page/copyright">Для правообладателей</FooterLink>
